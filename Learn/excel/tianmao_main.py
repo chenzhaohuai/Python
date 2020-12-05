a650add911e78ab8732ad0af57de410a0493f044
Dev_CZH <784723473@qq.com>
2020-12-05 17:01:04 +0800

    fix 数据抓取

--- Learn/excel/tianmao_main.py
+++ Learn/excel/tianmao_main.py
@@ -27,8 +27,45 @@ class Taobao:
         self.get_page_info()
 
     def get_page_info(self):
-        button = self.browser.page_source
-        print(button) 
+        response = self.browser.page_source.encode('utf-8')
+        # print(response)
+        print('-'*30)
+        soup = BeautifulSoup(response,'lxml')
+
+        shop_name_list = []
+        shop_price_list = []
+        shop_person_list = []
+        shop_location_list = []
+        soup_data_list = soup.find('div',class_='grid g-clearfix').find_all_next('div',class_='items') 
+        for shop_data in soup_data_list:
+            shop_image_data = shop_data.find_all('div',class_='pic')
+            for shop_data_a in shop_image_data:
+                shop_data_a = shop_data.find_all('a',class_='pic-link J_ClickStat J_ItemPicA')
+                for shop_name in shop_data_a:
+                    shop_name = shop_name.find_all('img')[0]['alt']
+                    shop_name_list.append(shop_name)
+
+			
+            #购买价格
+            shop_price_data = shop_data.find_all('div',class_='price g_price g_price-highlight')
+            for shop_price in shop_price_data:
+                shop_price_list.append(shop_price.text.strip())
+
+			#购买人数
+
+            shop_purchase_sum = shop_data.find_all('div',class_='deal-cnt')
+            for shop_purchase in shop_purchase_sum:
+                shop_person_list.append(shop_purchase.text.strip())
+
+			#购买地区
+            shop_location_data = shop_data.find_all('div',class_='location')
+            for shop_location in shop_location_data:
+                shop_location_list.append(shop_location.text.strip())
+
+			#打印产品信息元组
+            shop_data = zip(shop_name_list,shop_price_list,shop_person_list, shop_location_list)
+            for data in shop_data:
+            print(data)
 
 
 if __name__ == "__main__":
